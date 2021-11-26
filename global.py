89472d557dea3d1cc398a9c8298d0ef67f0ed448
Yaros5 <94068316+Yaros5@users.noreply.github.com>
2021-11-26 17:39:30 +0200

    com
    com

---
+++ global.py
@@ -0,0 +1,26 @@
+def binary_search(ser, item):
+    global guess, mid
+    low = 0
+    hig = len(ser) - 1
+
+    while low <= hig:
+        mid = (low + hig) // 2
+        guess = ser[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            hig = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 9))
+
+
+
+dict = {
+
+}
