3fe68379084847c73ee5d071d63db1c8073eeb3d
Oleshko <olehchoma@gmail.com>
2021-11-26 18:22:12 +0200

    bot

--- bot.py
+++ bot.py
@@ -1,11 +1,27 @@
 import telebot
 
-bot = telebot.TeleBot("2142529380:AAH9OvlYi9zcii99FWijDrQiVhwGOjuFRn0")
+
+# ! API
+TOKEN = "2142529380:AAH9OvlYi9zcii99FWijDrQiVhwGOjuFRn0"
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+# ! start
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привіт, {0.first_name}!👋'.format(
+        message.from_user, bot.get_me()), parse_mode='html')
+    bot.send_message(message.chat.id, 'Напиши своє ім\'я та прізвише')
+    name = message.text
+
+
+# ! changename
+@bot.message_handler(commands=['changename'])
+def changename(message):
+    bot.send_message(message.chat.id, 'Напиши своє ім\'я та прізвише')
+    name = message.text
+
 
 bot.infinity_polling()
