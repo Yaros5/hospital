f364180f32b2e1999e063761904f717143d90971
Oleshko <olehchoma@gmail.com>
2021-11-27 15:31:07 +0200

    фів

--- global.py
+++ global.py
@@ -2,7 +2,6 @@ def binary_search(ser, item):
     global guess, mid
     low = 0
     hig = len(ser) - 1
-
     while low <= hig:
         mid = (low + hig) // 2
         guess = ser[mid]
@@ -13,14 +12,7 @@ def binary_search(ser, item):
         else:
             low = mid + 1
     return None
-
-
 my_list = [1, 3, 5, 7, 9]
-
 print(binary_search(my_list, 9))
-
-
-
 dict = {
-
 }
