bb854ba79ba5285104e5d1aa343ee64fdd279df3
Oleshko <olehchoma@gmail.com>
2021-11-27 13:13:17 +0200

    bot eng

--- bot.py
+++ bot.py
@@ -11,35 +11,35 @@ def start_message(message):
     bot.remove_webhook()
     # remove keyboard
     remove_keyboard = types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, "*Привіт, {0.first_name} 👋*".format(message.from_user, bot.get_me()), reply_markup=remove_keyboard, parse_mode="Markdown")
-    bot.send_message(message.chat.id, "*Якщо ти лікар, то тут ти можеш переглянути розклад своїх пацієнтів*", parse_mode="Markdown")
+    bot.send_message(message.chat.id, "*Hello, {0.first_name} 👋*".format(message.from_user, bot.get_me()), reply_markup=remove_keyboard, parse_mode="Markdown")
+    bot.send_message(message.chat.id, "*If you are a doctor, then here you can view the schedule of your patients*", parse_mode="Markdown")
     ask_for_name(message)
 
 # ! ask for name
 def ask_for_name(message):
     # remove keyboard
     remove_keyboard = types.ReplyKeyboardRemove()
-    msg = bot.send_message(message.chat.id, "Напиши мені своє *ім'я* та *прізвище*", parse_mode="Markdown", reply_markup=remove_keyboard)
+    msg = bot.send_message(message.chat.id, "Write me your *name* and *last name*", parse_mode="Markdown", reply_markup=remove_keyboard)
     bot.register_next_step_handler(msg, verification)
 
 # ! verification
 def verification(message):
     # add inline
     markup_inline_yn = types.InlineKeyboardMarkup()
-    item_yes = types.InlineKeyboardButton(text="Так", callback_data="yes")
-    item_no = types.InlineKeyboardButton(text="Ні", callback_data="no")
+    item_yes = types.InlineKeyboardButton(text="Yes", callback_data="yes")
+    item_no = types.InlineKeyboardButton(text="No", callback_data="no")
     markup_inline_yn.add(item_yes, item_no)
-    bot.send_message(message.chat.id, f"*Тебе звати {message.text}?*", reply_markup=markup_inline_yn, parse_mode="Markdown")
+    bot.send_message(message.chat.id, f"*Your name is {message.text}?*", reply_markup=markup_inline_yn, parse_mode="Markdown")
 # ! verification_complete
 @bot.callback_query_handler(func=lambda call: True)
 def verification_complete(call):
     if call.data == "yes":
         # add keyboard
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item_change_name = types.KeyboardButton("✒️ Змінити ім'я")
-        item_schedule = types.KeyboardButton("📰 Розклад")
+        item_change_name = types.KeyboardButton("✒️ Change name")
+        item_schedule = types.KeyboardButton("📰 Schedule")
         keyboard.add(item_change_name, item_schedule)
-        bot.send_message(call.message.chat.id, "*Вибери, що робити далі*", reply_markup=keyboard, parse_mode="Markdown")
+        bot.send_message(call.message.chat.id, "*Choose what to do next*", reply_markup=keyboard, parse_mode="Markdown")
     elif call.data == "no":
         ask_for_name(call.message)
     # delete message
@@ -62,11 +62,11 @@ def buttons(message):
         bot.delete_message(message.chat.id, message.message_id)
         bot.send_message(message.chat.id, "*CHOSEN ONES ARE THE BEST!*", parse_mode="Markdown")
     # ! change name
-    elif message.text == "✒️ Змінити ім'я":
+    elif message.text == "✒️ Change name":
         ask_for_name(message)
     # ! schedule
-    elif message.text == "📰 Розклад":
-        bot.send_message(message.chat.id, "*Розклад твоїх пацієнтів:*", parse_mode="Markdown")
+    elif message.text == "📰 Schedule":
+        bot.send_message(message.chat.id, "*Your patients` schedule:*", parse_mode="Markdown")
         # example
         bot.send_message(message.chat.id, "Пацієнт 1")
         bot.send_message(message.chat.id, "Пацієнт 2")
